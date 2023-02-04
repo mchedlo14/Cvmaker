2418e1b47e57faf1eaa80b6fad8c463d221408d5
mchedlo14 <golocogo4@gmail.com>
2023-02-04 10:45:18 -0800

    added name and lname form

--- src/components/FirstForm/FirstForm.jsx
+++ src/components/FirstForm/FirstForm.jsx
@@ -15,6 +15,23 @@ const FirstForm = ({formCounter,setFormCounter}) => {
       <div className='nav-icon-wrapper' onClick={backToHomePage}>
         <img src={leftArrowIcon} alt='arrow icon'/>
       </div>
+
+      <div className='information-form'>
+        <div className='first-container'>
+          <div className='name-container'>
+            <h2>სახელი</h2>
+            <input placeholder='ანზორ'/>
+            <p>მინიმუმ 2 ასო, ქართული ასოები</p>
+          </div>
+
+          <div className='name-container'>
+            <h2>გვარი</h2>
+            <input placeholder='მუმლაზე'/>
+            <p>მინიმუმ 2 ასო, ქართული ასოები</p>
+          </div>
+
+        </div>
+      </div>
     </div>
   )
 }
